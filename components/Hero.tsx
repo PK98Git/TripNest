31d6c071fe4822edfe819050a41036f7ba62e318
Pasindu Kanchana <pkbiz1998@gmail.com>
2025-06-30 14:56:52 +0530

    modify css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
       <div className="hero-map" />
-      <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
+      <div className="relative z-20 flex flex-2 flex-col xl:w-1/2">
         <Image
           src="/camp.svg"
           alt="camp"
@@ -50,7 +50,7 @@ const Hero = () => {
           />
         </div>
       </div>
-      <div className="relative flex flex-1 items-start ">
+      <div className="relative flex flex-2 items-start ">
         <div className="relative z-10 w-[268px] flex flex-col gap-8 rounded-3xl bg-green-90 px-7 py-8">
           <div className="flex flex-col">
             <div className="flexBetween">
